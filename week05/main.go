740e8c6526bff2193bc43cdbefa9cf42cc45ff80
DoRaGee_ <ruy0218@naver,com>
2023-09-25 14:36:06 +0900

    week05 v0.4

--- week05/main.go
+++ week05/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
-	HotSpurs := "hm ? j madi?"
-	replacePlayer := strings.NewReplacer("?", "son")
-	player := replacePlayer.Replace(HotSpurs)
-	fmt.Println(player)
+	fmt.Print("Input score : ")
+	reader := bufio.NewReader(os.Stdin)        //메모리 번지 수를 reader가 가진다?
+	inputScore, err := reader.ReadString('\n') //err declared and not used
+	fmt.Println(inputScore)
 }
