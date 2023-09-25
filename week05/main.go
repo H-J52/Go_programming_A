9c5492062988c79c09e973b903e60b7e5795e5e1
DoRaGee_ <ruy0218@naver,com>
2023-09-25 14:41:39 +0900

    week05 v0.5

--- week05/main.go
+++ week05/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)      //메모리 번지 수를 reader가 가진다?
-	inputScore, _ := reader.ReadString('\n') //option 1
+	inputScore, _ := reader.ReadString('\n') //option 1, Igonore the error return value with the..
 	fmt.Println(inputScore)
 }
