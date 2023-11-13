2c7c644fba6d8e8be55b831e87f405d88ac140e3
DoRaGee_ <ruy0218@naver,com>
2023-11-13 15:31:20 +0900

    V.4

--- week12/main.go
+++ week12/main.go
@@ -3,6 +3,33 @@ package main
 import "fmt"
 
 func main() {
+
+	a := make([]string, 4, 5) //타입, 개수, 수용량
+	a[0] = "a"
+	a[2] = "c"
+	a[3] = "d"
+	as := a[0:2]
+	as[1] = "z"
+	c := append(a, "y", "x") //capacity가 5에서 10으로 바뀐다.
+
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(c, len(c), cap(c))
+
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
+	c[0] = "k"
+	fmt.Println(a, c)
+
+	// a := []string{"a", "b", "c", "d"}
+	// as := a[:2]
+	// as[1] = "z"
+
+	// fmt.Println(a)
+	// fmt.Println(as)
+
+	// b := [4]int{4, 3, 2, 1}
+	// bs := b[1:3]
+	// fmt.Println(bs)
+
 	// var s []int
 	// s = make([]int, 5)
 
@@ -12,24 +39,25 @@ func main() {
 	// for _, value := range s {
 	// 	fmt.Println(value)
 	// }
-	s := []int{0, 0, 0, 0, 0}
+	//=====================================
+	// s := []int{0, 0, 0, 0, 0}
 
-	s[4] = 99
-	s[2] = 91
+	// s[4] = 99
+	// s[2] = 91
 
-	for _, value := range s {
-		fmt.Println(value)
-	}
+	// for _, value := range Println(a)
+	// 	fmt.Println(value)
+	// }
 
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
+	// copyS := s[1:4]
+	// for _, value := range copyS {
+	// 	fmt.Println(value)
+	// }
 
-	test := [3]string{"inha", "go", "student"} //배열 리터럴을 이용해서 test 배열 생성
-	testS := test[0:2]
-	testS2 := test[1:]
-	testS2[0] = "python"
-	fmt.Println(testS2)
-	fmt.Println(testS, len(testS))
+	// test := [3]string{"inha", "go", "student"} //배열 리터럴을 이용해서 test 배열 생성
+	// testS := test[0:2]
+	// testS2 := test[1:]
+	// testS2[0] = "python"
+	// fmt.Println(testS2)
+	// fmt.Println(testS, len(testS))
 }
